deee72c7782e8e1cdfe0d42b6bb88f5f47ad3eac
Robert Graupner <robert.graupnerr@gmail.com>
2022-04-11 22:48:08 +0200

    add header and footer component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import EditPost from "./pages/EditPost/EditPost";
 import SinglePost from "./pages/SinglePost/SinglePost";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
+import Header from "./views/Header/Header";
+import Footer from "./views/Footer/Footer";
+import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
 	return (
-		<main>
+		<Container>
+			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/post/:id" element={<SinglePost />} />
@@ -17,7 +21,8 @@ const App = () => {
 				<Route path="/about" element={<About />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
-		</main>
+			<Footer />
+		</Container>
 	);
 };
 
