dd38c004c92b81702c9d8b050d25a9b0bac1eade
Robert Graupner <robert.graupnerr@gmail.com>
2022-05-05 22:21:54 +0200

    add form validation

--- src/features/PostForm/PostForm.js
+++ src/features/PostForm/PostForm.js
@@ -4,39 +4,63 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useForm } from "react-hook-form";
 
 const PostForm = ({ action, actionText, ...props }) => {
+
+	const {register, handleSubmit: validate, formState: { errors },} = useForm();
+
   const [title, setTitle] = useState(props.title || '');
   const [author, setAuthor] = useState(props.author || '');
   const [publishedDate, setPublishedDate] = useState(props.publishedDate || new Date());
   const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
 	const [content, setContent] = useState(props.content || '');
+	const [contentError, setContentError] = useState(false);
+  const [dateError, setDateError] = useState(false);
 
-   const handleSubmit = e => {
-    e.preventDefault();
-    action({ title, author, publishedDate, shortDescription, content });
-   };
+	const handleSubmit = e => {
+		 //mozna zrezygnowac z e.preventDefault() bo robi to juz funkcja validate
+    //e.preventDefault();
+   setContentError(!content)
+   setDateError(!publishedDate)
+   if(content && publishedDate) { 
+     action({ title, author, publishedDate, shortDescription, content });
+   }
+ };
   
-  return (
-		<Form onSubmit={handleSubmit}>
-			<Form.Group className="mb-3" controlId="formTitle">
+	return (
+		//gdy nastąpi event submit, zwaliduj formularz, jesli jest poprawny uruchom funkcje handlesubmit
+		<Form onSubmit={validate(handleSubmit)}>
+			<Form.Group className="mb-3" controlId="formBasicEmail">
 				<Form.Label>Title</Form.Label>
 				<Form.Control
-					type="text"
-					placeholder="Enter title"
+					{...register("title", { required: true, minLength: 3 })}
 					value={title}
 					onChange={(e) => setTitle(e.target.value)}
+					type="text"
+					placeholder="Enter title"
 				/>
+				{errors.title && (
+					<small className="d-block form-text text-danger mt-2">
+						This field is required and should have at least 3 characters
+					</small>
+				)}
 			</Form.Group>
 
 			<Form.Group className="mb-3" controlId="formAuthor">
 				<Form.Label>Author</Form.Label>
 				<Form.Control
+					{...register("author", { required: true, minLength: 3 })}
 					type="text"
 					placeholder="Enter author"
 					value={author}
 					onChange={(e) => setAuthor(e.target.value)}
 				/>
+				{errors.author && (
+					<small className="d-block form-text text-danger mt-2">
+						This field is required and should have at least 3 characters
+					</small>
+				)}
 			</Form.Group>
 
 			<Form.Group className="mb-3" controlId="formPublishedDate">
@@ -45,22 +69,42 @@ const PostForm = ({ action, actionText, ...props }) => {
 					selected={publishedDate}
 					onChange={(date) => setPublishedDate(date)}
 				/>
+				{dateError && (
+					<small className="d-block from-text text-danger mt-2">
+						Date can't be empty
+					</small>
+				)}
 			</Form.Group>
 
 			<Form.Group className="mb-3" controlId="formShordDescription">
 				<Form.Label>Short description</Form.Label>
 				<Form.Control
+					{...register("shortDescription", { required: true, minLength: 20 })}
 					as="textarea"
-					rows={6}
+					rows={3}
 					placeholder="Leave a comment here"
 					value={shortDescription}
 					onChange={(e) => setShortDescription(e.target.value)}
 				/>
+				{errors.shortDescription && (
+					<small className="d-block form-text text-danger mt-2">
+						This field is required and should have at least 20 characters
+					</small>
+				)}
 			</Form.Group>
 
 			<Form.Group className="mb-3" controlId="formMainContent">
 				<Form.Label>Main content</Form.Label>
-				<ReactQuill theme="snow" value={content} onChange={setContent} />
+				<ReactQuill
+					theme="snow"
+					value={content}
+					onChange={setContent}
+				/>
+				{contentError && (
+					<small className="d-block form-text text-danger mt-2">
+						Content can't be empty
+					</small>
+				)}
 			</Form.Group>
 
 			<Button variant="primary" type="submit">
